ea45856d86bb9385ea9533eeb527f9be0b992d3f
Kolavennu Sai Sreeram Kumar <sensational.sreeram999@gmail.com>
2023-03-14 23:35:17 +0530

    message[Sreeram] Program for Tables Using For Loop with different Multiples

---
+++ forloop/Program.cs
@@ -0,0 +1,16 @@
+﻿public  class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the number of table that you want to print");
+         int n = Convert.ToInt32(Console.ReadLine ());
+        Console.WriteLine("Enter the Muliple times of the Table you want to print ");
+         int k= Convert.ToInt32(Console.ReadLine());
+        int i;
+        for (i=1;i<=k;i++)
+        {
+            Console.WriteLine(" {0} X {1} = {2}", n,i,i*n);
+        }
+        
+    }
+}
